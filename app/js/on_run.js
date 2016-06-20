ed5227a721e949d143ff708ee88f68f5aa851f4a
Michael Benz <michael@benz.io>
2016-06-21 02:07:48 +1000

    moved philips hue logic to main controller as it was screwing with unit tests

--- app/js/on_run.js
+++ app/js/on_run.js
@@ -1,4 +1,4 @@
-function OnRun($rootScope, AppSettings, $mdDialog, $mdToast, ngHue, $localStorage) {
+function OnRun($rootScope, AppSettings) {
   'ngInject';
 
   // change page title based on state
@@ -12,51 +12,6 @@ function OnRun($rootScope, AppSettings, $mdDialog, $mdToast, ngHue, $localStorag
 
     $rootScope.pageTitle += AppSettings.appTitle;
   });
-  
-    function showHueSetup() {
-    // Appending dialog to document.body to cover sidenav in docs app
-    var confirm = $mdDialog.confirm()
-          .title('We found a Hue Bridge on your Network!')
-          .textContent('To enable Phillips Hue integration, please tap the button on your Bridge, then tap Done')
-          .ariaLabel('Enable Hue Integration')
-          .ok('Done')
-          .cancel('cancel');
-	    $mdDialog.show(confirm).then(function() {
-			_createHueUser();
-	    }, function() {
-			return;
-	    });
-  	};
-
-    if ($localStorage.HueUsername===undefined){
-  	  _createHueUser();
-    }
-	else {
-		connectToHue($localStorage.HueUsername);
-	}
-
-	function _createHueUser()
-	{
-	  	ngHue.createUser(AppSettings.HueAppName).then(function successCallback(response) {
-			$localStorage.HueUsername = response[0].success.username;
-			connectToHue($localStorage.HueUsername);
-	  	}, function errorCallback(response) {
-			if (response[0].error.type === 101) {
-				showHueSetup();
-			};
-	  	});
-	}
-
-	function connectToHue(username){
-		ngHue.setup({
-			username: username
-		})
-		$mdToast.show(
-		      $mdToast.simple()
-		        .textContent('connected to Hue Bridge')
-		        .hideDelay(3000)
-		    );
-	}
 
 }
 
