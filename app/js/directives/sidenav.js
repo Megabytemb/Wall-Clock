55354a0f197d04f1df2d78ee73682e286efce50e
Michael Benz <michael@benz.io>
2016-06-21 01:25:33 +1000

    Moved Sidenav to a directive.

---
+++ app/js/directives/sidenav.js
@@ -0,0 +1,23 @@
+function sideNavController()
+{
+	var vm = this;
+	
+	vm.temp='Hell World';
+
+}
+
+function sideNav() {
+
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/sidenav.html',
+  	controller: sideNavController,
+	controllerAs:'vm',
+	bindToController: true 
+  };
+}
+
+export default {
+  name: 'sideNav',
+  fn: sideNav
+};
